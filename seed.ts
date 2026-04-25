1e63eaefd56dc27fa247add11853f67aa4e522ab
Koshii <muhbarkah05@gmail.com>
2026-04-25 18:41:25 +0700

    fix seed

--- seed.ts
+++ seed.ts
@@ -3,9 +3,8 @@ import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import bcrypt from 'bcryptjs'
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!
-})
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
 
 async function main() {
